c6cde51dbc0c50c19b3734a175c267df3424be7f
Daniel Winterbottom <daniel.winterbottom@cern.ch>
2026-05-05 17:41:41 +0100

    adding code to produce reco taus and starting to add reco variables

--- tauentanglement/generation/run_delphes.py
+++ tauentanglement/generation/run_delphes.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+# IP and SV etc units are in mm
 
 import sys
 import ROOT
@@ -78,6 +79,132 @@ def get_impact_parameter(p):
     dz = impact_point_vec3.Z()
     return impact_point_vec3, d0, dz
 
+
+def GetMatchingRecoParticle(gen_vis, reco_particles, threshold=0.4):
+    matched_particles = []
+    for i in range(reco_particles.GetEntries()):
+        reco = reco_particles.At(i)
+        dR = gen_vis.DeltaR(reco.P4())
+        if dR < threshold:
+            matched_particles.append(reco)
+    return matched_particles
+
+def GetPi0(gammas):
+    # get pi0 energy by summing energies of gammas
+    # get direction by taking energy weighted eta and phi of gammas
+    # set mass to mass of pi0
+    E = 0 
+    eta = 0
+    phi = 0
+    for g in gammas:
+        E += g.E
+        eta += g.E * g.Eta
+        phi += g.E * g.Phi
+    if E > 0:
+        eta /= E
+        phi /= E
+        pi0 = ROOT.TLorentzVector()
+        pi0.SetPtEtaPhiM(E / math.cosh(eta), eta, phi, 0.1349)
+        return pi0
+    else:
+        return ROOT.TLorentzVector()
+
+def RecoStrips(cands_, stripPtThreshold=2.5):
+
+    #sort cands by pT (highest pT first)
+    cands_.sort(key=lambda x: x.ET, reverse=True)
+
+    # remove candidates that dont pass pT-eta cuts
+    cands_ = [c for c in cands_ if c.ET > 1 and abs(c.Eta) < 2.5]
+
+    strips = []
+
+    while len(cands_) > 0:
+        associated = []
+        notAssociated = []
+
+        stripVector = cands_[0].P4()
+        associated.append(cands_[0])
+        cands_ = cands_[1:]
+
+        # loop and associate other candidates
+        repeat = True
+        while repeat:
+            repeat = False
+            for c in cands_:
+                etaAssociationDistance = min(max(0.2*pow(c.ET, -0.66) + 0.2*pow(stripVector.Pt(), -0.66), 0.05), 0.15)
+                phiAssociationDistance = min(max(0.35*pow(c.ET, -0.71) + 0.35*pow(stripVector.Pt(), -0.71), 0.05), 0.30)
+                if abs(c.Eta - stripVector.Eta()) < etaAssociationDistance and abs(ROOT.Math.VectorUtil.DeltaPhi(c.P4(), stripVector)) < phiAssociationDistance:
+                    stripVector += c.P4()
+                    associated.append(c)
+                    repeat = True
+                else:
+                    notAssociated.append(c)
+
+            # swap the candidate vector with the non associated vector
+            cands_ = notAssociated
+            notAssociated = []
+
+        strip = GetPi0(associated)
+        if strip.Pt() >= stripPtThreshold:
+            strips.append((strip, associated))
+
+    return strips
+
+def CheckWithinSigCone(tau_cand):
+    tau_vis = tau_cand[0]
+    cands = tau_cand[1]+tau_cand[2]
+    Rsig = max(min(3.0/tau_vis.Pt(),0.1),0.05)
+
+    for c in cands:
+        if isinstance(c, ROOT.TLorentzVector):
+            c_p4 = c
+        else:
+            c_p4 = c.P4()
+        if tau_vis.DeltaR(c_p4) > Rsig:
+            return False
+    return True
+
+def GetTauCands(tracks, strips, incDM2=True):
+    #apply pT and eta cuts to tracks
+    tracks_ = [t for t in tracks if t.PT > 0.5 and abs(t.Eta) < 2.5]
+
+    # first make all combination of 1 track and 0,1,2 strips
+    tau_cands = []
+    for t in tracks_:
+        tau_vis = t.P4()
+        tau_cands.append([tau_vis, [t], []]) # 1 track, 0 strips
+        #TODO: might need to add the additional delta_mass part to the min and max masses below
+        for s in strips:
+            mass_2body = (t.P4() + s[0]).M()
+            pT_2body = (t.P4() + s[0]).Pt()
+            mass_2body_min = 0.3
+            mass_2body_max = max(min(1.3 * math.sqrt(pT_2body/100), 4.2), 1.3)
+            if mass_2body > mass_2body_min and mass_2body < mass_2body_max:
+                tau_vis = t.P4() + s[0]
+                tau_cands.append([tau_vis, [t], [s[0]]]) # 1 track, 1 strip
+            if incDM2:
+                for s2 in strips:
+                    if s2 != s:
+                        mass_3body = (t.P4() + s[0] + s2[0]).M()
+                        pT_3body = (t.P4() + s[0] + s2[0]).Pt()
+                        mass_3body_min = 0.4
+                        mass_3body_max = max(min(1.2 * math.sqrt(pT_3body/100), 4.0), 1.2)
+                        if mass_3body > mass_3body_min and mass_3body < mass_3body_max:
+                            tau_vis = t.P4() + s[0] + s2[0]
+                            tau_cands.append([tau_vis, [t], [s[0], s2[0]]]) # 1 track, 2 strips
+
+    # filter any tau_cands not passing signal cone requirement
+    tau_cands = [c for c in tau_cands if CheckWithinSigCone(c)]
+
+    #sort tau_cands by pT
+    tau_cands.sort(key=lambda x: x[0].Pt(), reverse=True)
+
+    return tau_cands
+
+
+    
+
 parser = argparse.ArgumentParser()
 parser.add_argument("--output" ,'-o', help="Name of output file")
 parser.add_argument("--input" ,'-i', help="Name of input file")
@@ -94,8 +221,9 @@ reader = ROOT.ExRootTreeReader(chain)
 particles = reader.UseBranch("Particle") # gen particles 
 MET = reader.UseBranch("MissingET")
 genMET = reader.UseBranch("GenMissingET")
-photon = reader.UseBranch("EFlowPhoton")
-eflowtrack = reader.UseBranch("EFlowTrack")
+photons = reader.UseBranch("EFlowPhoton")
+eflowtracks = reader.UseBranch("EFlowTrack")
+tracks = reader.UseBranch("Track")
 
 #setup output tree
 
@@ -103,6 +231,8 @@ branches = [
   'taup_npi', 'taup_npizero',
   'taun_npi', 'taun_npizero',
   'met_px', 'met_py',
+  'taup_vis_pT', 'taup_vis_eta',
+  'taun_vis_pT', 'taun_vis_eta',
   'taup_pi1_px', 'taup_pi1_py', 'taup_pi1_pz', 'taup_pi1_e',
   'taup_pi1_ipx', 'taup_pi1_ipy', 'taup_pi1_ipz',
   'taup_pi2_px', 'taup_pi2_py', 'taup_pi2_pz', 'taup_pi2_e',
@@ -118,7 +248,7 @@ branches = [
   'taup_sv_x', 'taup_sv_y', 'taup_sv_z',
   'taun_sv_x', 'taun_sv_y', 'taun_sv_z',
   'taup_nu_px', 'taup_nu_py', 'taup_nu_pz',
-  'taun_nu_px', 'taun_nu_py', 'taun_nu_pz'
+  'taun_nu_px', 'taun_nu_py', 'taun_nu_pz',
 ]
 
 fout = ROOT.TFile(args.output,'RECREATE')
@@ -128,6 +258,10 @@ branch_vals = {}
 for b in branches:
     branch_vals[b] = array('f',[0])
     tree.Branch(b,  branch_vals[b],  '%s/F' % b)
+    # add a reco branch for everything except the nus
+    if not b.startswith('taup_nu') and not b.startswith('taun_nu'):
+        branch_vals['reco_' + b] = array('f',[0])
+        tree.Branch('reco_' + b,  branch_vals['reco_' + b],  'reco_%s/F' % b)
 
 
 for iev in range(reader.GetEntries()):
@@ -248,9 +382,15 @@ for iev in range(reader.GetEntries()):
 #]
 
     branch_vals['taup_npi'][0] = len(taup_pis)
-    branch_vals['taup_npizero'][0] = len(taup_gammas)/2 # 2 gammas per pi0
+    branch_vals['taup_npizero'][0] = len(taup_gammas)//2 # 2 gammas per pi0 - round down if odd number of gammas
     branch_vals['taun_npi'][0] = len(taun_pis)
-    branch_vals['taun_npizero'][0] = len(taun_gammas)/2 # 2 gammas per pi0
+    branch_vals['taun_npizero'][0] = len(taun_gammas)//2 # 2 gammas per pi0
+
+    branch_vals['taup_vis_pT'][0] = taup_vis.Pt()
+    branch_vals['taup_vis_eta'][0] = taup_vis.Eta()
+    branch_vals['taun_vis_pT'][0] = taun_vis.Pt()
+    branch_vals['taun_vis_eta'][0] = taun_vis.Eta()
+
     branch_vals['met_px'][0] = genMET.At(0).MET * math.cos(genMET.At(0).Phi)
     branch_vals['met_py'][0] = genMET.At(0).MET * math.sin(genMET.At(0).Phi)
     branch_vals['taup_pizero1_px'][0] = taup_pi0.Px()
@@ -261,12 +401,14 @@ for iev in range(reader.GetEntries()):
     branch_vals['taun_pizero1_py'][0] = taun_pi0.Py()
     branch_vals['taun_pizero1_pz'][0] = taun_pi0.Pz()
     branch_vals['taun_pizero1_e'][0] = taun_pi0.E()
+
     branch_vals['taup_nu_px'][0] = taup_neutrinos_sum.Px()
     branch_vals['taup_nu_py'][0] = taup_neutrinos_sum.Py()
     branch_vals['taup_nu_pz'][0] = taup_neutrinos_sum.Pz()
     branch_vals['taun_nu_px'][0] = taun_neutrinos_sum.Px()
     branch_vals['taun_nu_py'][0] = taun_neutrinos_sum.Py()
     branch_vals['taun_nu_pz'][0] = taun_neutrinos_sum.Pz()
+
     branch_vals['taup_lep_px'][0] = taup_leptons[0].P4().Px() if len(taup_leptons) > 0 else 0
     branch_vals['taup_lep_py'][0] = taup_leptons[0].P4().Py() if len(taup_leptons) > 0 else 0
     branch_vals['taup_lep_pz'][0] = taup_leptons[0].P4().Pz() if len(taup_leptons) > 0 else 0
@@ -280,8 +422,100 @@ for iev in range(reader.GetEntries()):
     branch_vals['taup_pi1_py'][0] = taup_pis[0].P4().Py() if len(taup_pis) > 0 else 0
     branch_vals['taup_pi1_pz'][0] = taup_pis[0].P4().Pz() if len(taup_pis) > 0 else 0
     branch_vals['taup_pi1_e'][0] = taup_pis[0].P4().E() if len(taup_pis) > 0 else 0
+    branch_vals['taun_pi1_px'][0] = taun_pis[0].P4().Px() if len(taun_pis) > 0 else 0
+    branch_vals['taun_pi1_py'][0] = taun_pis[0].P4().Py() if len(taun_pis) > 0 else 0
+    branch_vals['taun_pi1_pz'][0] = taun_pis[0].P4().Pz() if len(taun_pis) > 0 else 0
+    branch_vals['taun_pi1_e'][0] = taun_pis[0].P4().E() if len(taun_pis) > 0 else 0
+
+    taup_ip = get_impact_parameter(taup_pis[0]) if len(taup_pis) > 0 else (ROOT.TVector3(0,0,0), 0, 0)
+    branch_vals['taup_pi1_ipx'][0] = taup_ip[0].X()
+    branch_vals['taup_pi1_ipy'][0] = taup_ip[0].Y()
+    branch_vals['taup_pi1_ipz'][0] = taup_ip[0].Z()
+
+    taun_ip = get_impact_parameter(taun_pis[0]) if len(taun_pis) > 0 else (ROOT.TVector3(0,0,0), 0, 0)
+    branch_vals['taun_pi1_ipx'][0] = taun_ip[0].X()
+    branch_vals['taun_pi1_ipy'][0] = taun_ip[0].Y()
+    branch_vals['taun_pi1_ipz'][0] = taun_ip[0].Z()
+
+    # get SVs but only if 3-prongs 
+    taup_sv = ROOT.TVector3(taup.X, taup.Y, taup.Z) if len(taup_pis) > 2 else ROOT.TVector3(0,0,0)
+    branch_vals['taup_sv_x'][0] = taup_sv.X()
+    branch_vals['taup_sv_y'][0] = taup_sv.Y()
+    branch_vals['taup_sv_z'][0] = taup_sv.Z()
+
+    taun_sv = ROOT.TVector3(taun.X, taun.Y, taun.Z) if len(taun_pis) > 2 else ROOT.TVector3(0,0,0)
+    branch_vals['taun_sv_x'][0] = taun_sv.X()
+    branch_vals['taun_sv_y'][0] = taun_sv.Y()
+    branch_vals['taun_sv_z'][0] = taun_sv.Z()
+
+    # get smeared "reco" quantities
+    # first we dR match reco particles to gen visible taus
+    taup_reco_track_matches = GetMatchingRecoParticle(taup_vis, eflowtracks, threshold=0.5)
+    taup_reco_photon_matches = GetMatchingRecoParticle(taup_vis, photons, threshold=0.5)
+    taun_reco_track_matches = GetMatchingRecoParticle(taun_vis, eflowtracks, threshold=0.5) # add in the track matches as well (these will have some smearing but also have charge and impact parameter info which is useful for the study)
+    taun_reco_photon_matches = GetMatchingRecoParticle(taun_vis, photons, threshold=0.5)
+
+    taup_strips = RecoStrips(taup_reco_photon_matches)
+    # sort by pT of the strip
+    taup_strips.sort(key=lambda x: x[0].Pt(), reverse=True)
+
+    taun_strips = RecoStrips(taun_reco_photon_matches)
+    # sort by pT of the strip
+    taun_strips.sort(key=lambda x: x[0].Pt(), reverse=True)
 
 
+
+    # print these out
+    print(f"\n\nEvent {iev}:")
+    print(f"  Gen taup: pT={taup.PT:.2f}, eta={taup.Eta:.2f}, phi={taup.Phi:.2f}, status={taup.Status}, charge={taup.Charge}, X={taup.X:.2f}, Y={taup.Y:.2f}, Z={taup.Z:.2f}")
+    print(f"  Gen vis taup: pT={taup_vis.Pt():.5f}, eta={taup_vis.Eta():.5f}, phi={taup_vis.Phi():.5f}, NPiZero={len(taup_gammas)/2:.0f}")
+    #print gen pis and photons
+    for i, pi in enumerate(taup_pis):
+        print(f"    Gen taup pi {i}: pT={pi.PT:.5f}, eta={pi.Eta:.5f}, phi={pi.Phi:.5f}, ip_x = {taup_ip[0].X()}, ip_y = {taup_ip[0].Y()}, ip_z = {taup_ip[0].Z()}")
+    for i, g in enumerate(taup_gammas):
+        print(f"    Gen taup gamma {i}: pT={g.PT:.5f}, eta={g.Eta:.5f}, phi={g.Phi:.5f}")
+    for i, reco in enumerate(taup_reco_track_matches):
+        print(f"  Reco track match {i}: pT={reco.PT:.5f}, eta={reco.Eta:.5f}, phi={reco.Phi:.5f}, charge={reco.Charge}, Xd={reco.Xd}, Yd={reco.Yd}, Zd={reco.Zd}, D0={reco.D0}, DZ={reco.DZ}, PID={reco.PID}, dR={taup_vis.DeltaR(reco.P4()):.5f}")
+    for i, reco in enumerate(taup_reco_photon_matches):
+        print(f"  Reco photon match {i}: pT={reco.ET:.5f}, eta={reco.Eta:.5f}, phi={reco.Phi:.5f}, dR={taup_vis.DeltaR(reco.P4()):.5f}")
+    print("N strips: ", len(taup_strips))
+    if len(taup_strips) > 0:
+        print(f"  Reco strips:")
+        for i, strip in enumerate(taup_strips):
+            print(f"    Strip {i}: pT={strip[0].Pt():.5f}, eta={strip[0].Eta():.5f}, phi={strip[0].Phi():.5f}, dR={taup_vis.DeltaR(strip[0]):.5f}, nGammas={len(strip[1])}")
+
+    taup_cands = GetTauCands(taup_reco_track_matches, taup_strips, incDM2=False) # Note: not allowing DM2 taus for now to match CMS reco where DM2 is very rare 
+    reco_taup_vis = taup_cands[0][0] if len(taup_cands) > 0 else ROOT.TLorentzVector()
+    taun_cands = GetTauCands(taun_reco_track_matches, taun_strips, incDM2=False)
+    reco_taun_vis = taun_cands[0][0] if len(taun_cands) > 0 else ROOT.TLorentzVector()
+
+    branch_vals['reco_taup_vis_pT'][0] = reco_taup_vis.Pt()
+    branch_vals['reco_taup_vis_eta'][0] = reco_taup_vis.Eta()
+    branch_vals['reco_taun_vis_pT'][0] = reco_taun_vis.Pt()
+    branch_vals['reco_taun_vis_eta'][0] = reco_taun_vis.Eta()
+
+    # TODO: implement 3-prongs at some point - the below assumes only dm=0 and dm=1 are present
+    if len(taup_cands) > 0:
+        branch_vals['reco_taup_npi'][0] = 1
+        branch_vals['reco_taup_npizero'][0] = len(taup_cands[0][1]) - 1 # number of strips is number of candidates - 1 (since one candidate is the track
+        branch_vals['reco_taup_pi1_px'][0] = taup_cands[0][1][0].P4().Px()
+        branch_vals['reco_taup_pi1_py'][0] = taup_cands[0][1][0].P4().Py()
+        branch_vals['reco_taup_pi1_pz'][0] = taup_cands[0][1][0].P4().Pz()
+        branch_vals['reco_taup_pi1_e'][0] = taup_cands[0][1][0].P4().E()
+    
+
+    #print out the tau candidates
+    print(f"  Tau candidates:")
+    for i, cand in enumerate(taup_cands):
+        print(f"    Candidate {i}: pT={cand[0].Pt():.5f}, eta={cand[0].Eta():.5f}, phi={cand[0].Phi():.5f}, nStrips={len([c for c in cand[2] if isinstance(c, ROOT.TLorentzVector)])}, dR={taup_vis.DeltaR(cand[0]):.5f}")
+
+
+    #store reco MET
+    branch_vals['reco_met_px'][0] = MET.At(0).MET * math.cos(MET.At(0).Phi)
+    branch_vals['reco_met_py'][0] = MET.At(0).MET * math.sin(MET.At(0).Phi)
 
     tree.Fill()
+
+fout.Write()
+fout.Close()
     
