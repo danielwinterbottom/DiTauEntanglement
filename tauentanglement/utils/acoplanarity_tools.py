792ab5e9c8ba980afd16c6e7b8a22e47fc7eaa65
lucasrussell01 <lucascharlesrussell@gmail.com>
2026-05-05 15:12:28 +0100

    tidy and vectorise code

--- tauentanglement/utils/acoplanarity_tools.py
+++ tauentanglement/utils/acoplanarity_tools.py
@@ -7,75 +7,166 @@ def spatial(v):
     p3 = ak.zip({"x": v.px, "y": v.py, "z": v.pz}, with_name="Vector3D")
     return p3, v.E
 
-def get_R_P_vectors(df, dm, tau_prefix = 'tau'):
-    if dm in [0, 1]:
-        # P is the charged pion momentum
-        P = ak.zip({"px": df[f"{tau_prefix}_pi1_px"], "py": df[f"{tau_prefix}_pi1_py"], "pz": df[f"{tau_prefix}_pi1_pz"], "E": df[f"{tau_prefix}_pi1_e"]}, with_name="Momentum4D")
-    if dm == 0:
-        # R is the impact parameter vector
-        R = ak.zip({"px": df[f"{tau_prefix}_pi1_ipx"], "py": df[f"{tau_prefix}_pi1_ipy"], "pz": df[f"{tau_prefix}_pi1_ipz"], "E": ak.zeros_like(df[f"{tau_prefix}_pi1_ipx"])}, with_name="Momentum4D")
-    elif dm == 1:
-        # R is the neutral pion
-        R = ak.zip({"px": df[f"{tau_prefix}_pizero1_px"], "py": df[f"{tau_prefix}_pizero1_py"], "pz": df[f"{tau_prefix}_pizero1_pz"], "E": df[f"{tau_prefix}_pizero1_e"]}, with_name="Momentum4D")
+def boost_vec(v):
+    return ak.zip({"x": v.px / v.E, "y": v.py / v.E, "z": v.pz / v.E}, with_name="Vector3D")
 
-    print("Successfully computed R and P vectors")
-    return R, P
+def polarimetric_vec_dm0(H_pi, boost_vec_tau):
+    return spatial(H_pi.boost(-boost_vec_tau))[0].unit()
 
-# first test for
-def compute_acoplanarity_angle(R1, P1, R2, P2, method_leg1, method_leg2, debug=False):
+def polarimetric_vec_dm1(H_pi, H_pizero, H_tau, boost_vec_tau):
+    """Compute the DM1 (rho) polarimetric vector in the tau rest frame."""
+    q = H_pi - H_pizero
+    P = H_tau
+    N = H_tau - H_pi - H_pizero
+    qN = q.dot(N)
+    qP = q.dot(P)
+    NP = N.dot(P)
+    q2 = q.dot(q)
+    massP = np.sqrt(np.maximum(P.dot(P), 0.0))
+    coeff = massP / (2 * qN * qP - q2 * NP)
+    pv = coeff * (2 * qN * q - q2 * N)
+    return spatial(pv.boost(-boost_vec_tau))[0].unit()
 
-    print(">>> Computing acoplanarity angle with method:", method_leg1, method_leg2)
-    sum_visible = P1 + P2 # old code used .boostvec, hopefully works
-    boost_vec  = ak.zip({"x": sum_visible.px / sum_visible.E, "y": sum_visible.py / sum_visible.E, "z": sum_visible.pz / sum_visible.E}, with_name="Vector3D")
+def get_R_P_vectors_all(df, tau_prefix='tau'):
+    """Compute R and P vectors for all events, selecting IP (DM0) or pizero (DM1) for R."""
+    P = ak.zip({
+        "px": df[f"{tau_prefix}_pi1_px"],
+        "py": df[f"{tau_prefix}_pi1_py"],
+        "pz": df[f"{tau_prefix}_pi1_pz"],
+        "E":  df[f"{tau_prefix}_pi1_e"],
+    }, with_name="Momentum4D")
 
-    if debug:
-        print("Before boost:")
-        print("P1:", P1)
-        print("P2:", P2)
-        print("R1:", R1)
-        print("R2:", R2)
-        print('*'*60, '\n')
-        print("Boost characteristics:")
-        print("sum_visible:", sum_visible)
-        print("boost_vec:",  boost_vec)
-        print('*'*60, '\n')
+    R_ip = ak.zip({
+        "px": df[f"{tau_prefix}_pi1_ipx"],
+        "py": df[f"{tau_prefix}_pi1_ipy"],
+        "pz": df[f"{tau_prefix}_pi1_ipz"],
+        "E":  ak.zeros_like(df[f"{tau_prefix}_pi1_ipx"]),
+    }, with_name="Momentum4D")
 
+    R_pizero = ak.zip({
+        "px": df[f"{tau_prefix}_pizero1_px"],
+        "py": df[f"{tau_prefix}_pizero1_py"],
+        "pz": df[f"{tau_prefix}_pizero1_pz"],
+        "E":  df[f"{tau_prefix}_pizero1_e"],
+    }, with_name="Momentum4D")
 
-    # get spatial components of R and P vectors in the rest frame of the di-tau system
-    R1_boosted_p3, R1_boosted_E  = spatial(R1.boost(-boost_vec))
-    R2_boosted_p3, R2_boosted_E  = spatial(R2.boost(-boost_vec))
-    P1_boosted_p3, P1_boosted_E = spatial(P1.boost(-boost_vec))
-    P2_boosted_p3, P2_boosted_E = spatial(P2.boost(-boost_vec))
+    is_dm1 = df[f"{tau_prefix}_haspizero"].values == 1
+    return ak.where(is_dm1, R_pizero, R_ip), P, is_dm1
 
-    if debug:
-        print("After boost:")
-        print("P1_boosted_p3, E", P1_boosted_p3, P1_boosted_E)
-        print("P2_boosted_p3, E", P2_boosted_p3, P2_boosted_E)
-        print("R1_boosted_p3: E", R1_boosted_p3, R1_boosted_E)
-        print("R2_boosted_p3: E", R2_boosted_p3, R2_boosted_E)
-        print('*'*60, '\n')
-
-    # compute the components of R that are perpendicular to P
-    R1perp = R1_boosted_p3 - ((R1_boosted_p3.dot(P1_boosted_p3))/(P1_boosted_p3.dot(P1_boosted_p3))) * P1_boosted_p3
-    R2perp = R2_boosted_p3 - ((R2_boosted_p3.dot(P2_boosted_p3))/(P2_boosted_p3.dot(P2_boosted_p3))) * P2_boosted_p3
-
-    # compute phi*
+def compute_aco_polarimetric(R1, P1, R2, P2, dm_taup, dm_taun):
+    R1perp = R1 - ((R1.dot(P1)) / (P1.dot(P1))) * P1
+    R2perp = R2 - ((R2.dot(P2)) / (P2.dot(P2))) * P2
     angle = np.arccos((R1perp.unit()).dot(R2perp.unit()))
-    # compute O*
-    sign = (P2_boosted_p3.unit()).dot((R1perp.unit()).cross(R2perp.unit()))
+    sign = (P2.unit()).dot((R1perp.unit()).cross(R2perp.unit()))
+    angle = ak.where(sign >= 0, angle, 2 * np.pi - angle)
+    return angle
 
-    # compute phiCP
+def compute_aco_classic(R1, P1, R2, P2, leg1_is_dp, leg2_is_dp):
+    """Compute the acoplanarity angle using IP (DM0) or DP (DM1) method per event."""
+    # Boost to visible charged decay product frame
+    bv = boost_vec(P1 + P2)
+
+    R1_p3, R1_E = spatial(R1.boost(-bv))
+    R2_p3, R2_E = spatial(R2.boost(-bv))
+    P1_p3, P1_E = spatial(P1.boost(-bv))
+    P2_p3, P2_E = spatial(P2.boost(-bv))
+
+    # Get perpendicular components
+    R1perp = R1_p3 - ((R1_p3.dot(P1_p3)) / (P1_p3.dot(P1_p3))) * P1_p3
+    R2perp = R2_p3 - ((R2_p3.dot(P2_p3)) / (P2_p3.dot(P2_p3))) * P2_p3
+
+    # Define angle between planes and O*, then apply appropriate shift
+    angle = np.arccos((R1perp.unit()).dot(R2perp.unit()))
+    sign  = (P2_p3.unit()).dot((R1perp.unit()).cross(R2perp.unit()))
     angle = ak.where(sign >= 0, angle, 2 * np.pi - angle)
 
-    # additional shifts for meson polarisation
-    meson_sign = ak.ones_like(angle)
-    if method_leg1 == 'DP':
-        Y1 = (R1_boosted_E - P1_boosted_E) / (P1_boosted_E + R1_boosted_E)
-        meson_sign = meson_sign * np.sign(Y1)
-    if method_leg2 == 'DP':
-        Y2 = (R2_boosted_E - P2_boosted_E) / (P2_boosted_E + R2_boosted_E)
-        meson_sign = meson_sign * np.sign(Y2)
-    if method_leg1 == 'DP' or method_leg2 == 'DP':
-        angle = ak.where(meson_sign < 0, ak.where(angle < np.pi, angle + np.pi, angle - np.pi), angle)
-
+    # Apply shift for polarisation of mesons
+    Y1 = (R1_E - P1_E) / (P1_E + R1_E)
+    Y2 = (R2_E - P2_E) / (P2_E + R2_E)
+    meson_sign = ak.where(leg1_is_dp, np.sign(Y1), ak.ones_like(angle))
+    meson_sign = meson_sign * ak.where(leg2_is_dp, np.sign(Y2), ak.ones_like(angle))
+    needs_shift = (leg1_is_dp | leg2_is_dp) & (meson_sign < 0)
+    angle = ak.where(needs_shift, ak.where(angle < np.pi, angle + np.pi, angle - np.pi), angle)
     return angle
+
+
+def get_ditau_polarimetric_gen(df):
+    """Compute the R and P vectors in the gen case (polarimetric vector and full tau)"""
+    # Build tau plus from decay products
+    pi_p     = ak.zip({"px": df["taup_pi1_px"], "py": df["taup_pi1_py"], "pz": df["taup_pi1_pz"], "E": df["taup_pi1_e"]}, with_name="Momentum4D")
+    nu_p     = ak.zip({"px": df["taup_nu_px"], "py": df["taup_nu_py"], "pz": df["taup_nu_pz"], "E": np.sqrt(df["taup_nu_px"]**2 + df["taup_nu_py"]**2 + df["taup_nu_pz"]**2)}, with_name="Momentum4D")
+    pizero_p = ak.zip({"px": df["taup_pizero1_px"], "py": df["taup_pizero1_py"], "pz": df["taup_pizero1_pz"], "E": df["taup_pizero1_e"]}, with_name="Momentum4D")
+    taup_is_dm1 = df["taup_haspizero"].values == 1
+    tau_p = ak.where(taup_is_dm1, pi_p + nu_p + pizero_p, pi_p + nu_p)
+
+    # Build tau minus from decay products
+    pi_n     = ak.zip({"px": df["taun_pi1_px"], "py": df["taun_pi1_py"], "pz": df["taun_pi1_pz"], "E": df["taun_pi1_e"]}, with_name="Momentum4D")
+    nu_n     = ak.zip({"px": df["taun_nu_px"], "py": df["taun_nu_py"], "pz": df["taun_nu_pz"], "E": np.sqrt(df["taun_nu_px"]**2 + df["taun_nu_py"]**2 + df["taun_nu_pz"]**2)}, with_name="Momentum4D")
+    pizero_n = ak.zip({"px": df["taun_pizero1_px"], "py": df["taun_pizero1_py"], "pz": df["taun_pizero1_pz"], "E": df["taun_pizero1_e"]}, with_name="Momentum4D")
+    taun_is_dm1 = df["taun_haspizero"].values == 1
+    tau_n = ak.where(taun_is_dm1, pi_n + nu_n + pizero_n, pi_n + nu_n)
+
+    # Boost to Higgs rest frame
+    higgs_bv = boost_vec(tau_p + tau_n)
+
+    H_tau_p = tau_p.boost(-higgs_bv)
+    H_tau_n = tau_n.boost(-higgs_bv)
+    H_pi_p  = pi_p.boost(-higgs_bv)
+    H_pi_n  = pi_n.boost(-higgs_bv)
+    H_piz_p = pizero_p.boost(-higgs_bv)
+    H_piz_n = pizero_n.boost(-higgs_bv)
+
+    # Compute polarimetric vectors in the respective tau rest frames
+    bv_taup = boost_vec(H_tau_p)
+    bv_taun = boost_vec(H_tau_n)
+
+    taup_s = ak.where(taup_is_dm1,
+        polarimetric_vec_dm1(H_pi_p, H_piz_p, H_tau_p, bv_taup),
+        polarimetric_vec_dm0(H_pi_p, bv_taup),
+    )
+    taun_s = ak.where(taun_is_dm1,
+        polarimetric_vec_dm1(H_pi_n, H_piz_n, H_tau_n, bv_taun),
+        polarimetric_vec_dm0(H_pi_n, bv_taun),
+    )
+
+    return taup_s, spatial(H_tau_p)[0].unit(), taun_s, spatial(H_tau_n)[0].unit()
+
+
+def get_ditau_polarimetric_reco(df):
+    """Compute the R and P vectors in the reco case using regressed neutrinos."""
+    # Define positive tau vectors
+    pi_p     = ak.zip({"px": df["taup_pi1_px"], "py": df["taup_pi1_py"], "pz": df["taup_pi1_pz"], "E": df["taup_pi1_E"]}, with_name="Momentum4D")
+    tau_p    = ak.zip({"px": df["alt_pred_tau_plus_px"], "py": df["alt_pred_tau_plus_py"], "pz": df["alt_pred_tau_plus_pz"], "E": df["alt_pred_tau_plus_E"]}, with_name="Momentum4D")
+    pizero_p = ak.zip({"px": df["taup_pizero1_px"], "py": df["taup_pizero1_py"], "pz": df["taup_pizero1_pz"], "E": df["taup_pizero1_E"]}, with_name="Momentum4D")
+    taup_is_dm1 = df["taup_haspizero"].values == 1
+
+    # Define negative tau vectors
+    pi_n     = ak.zip({"px": df["taun_pi1_px"], "py": df["taun_pi1_py"], "pz": df["taun_pi1_pz"], "E": df["taun_pi1_E"]}, with_name="Momentum4D")
+    tau_n    = ak.zip({"px": df["alt_pred_tau_minus_px"], "py": df["alt_pred_tau_minus_py"], "pz": df["alt_pred_tau_minus_pz"], "E": df["alt_pred_tau_minus_E"]}, with_name="Momentum4D")
+    pizero_n = ak.zip({"px": df["taun_pizero1_px"], "py": df["taun_pizero1_py"], "pz": df["taun_pizero1_pz"], "E": df["taun_pizero1_E"]}, with_name="Momentum4D")
+    taun_is_dm1 = df["taun_haspizero"].values == 1
+
+    # Boost to Higgs rest frame
+    higgs_bv = boost_vec(tau_p + tau_n)
+
+    H_tau_p = tau_p.boost(-higgs_bv)
+    H_tau_n = tau_n.boost(-higgs_bv)
+    H_pi_p  = pi_p.boost(-higgs_bv)
+    H_pi_n  = pi_n.boost(-higgs_bv)
+    H_piz_p = pizero_p.boost(-higgs_bv)
+    H_piz_n = pizero_n.boost(-higgs_bv)
+
+    # Compute polarimetric vectors in the respective tau rest frames
+    bv_taup = boost_vec(H_tau_p)
+    bv_taun = boost_vec(H_tau_n)
+
+    taup_s = ak.where(taup_is_dm1,
+        polarimetric_vec_dm1(H_pi_p, H_piz_p, H_tau_p, bv_taup),
+        polarimetric_vec_dm0(H_pi_p, bv_taup),
+    )
+    taun_s = ak.where(taun_is_dm1,
+        polarimetric_vec_dm1(H_pi_n, H_piz_n, H_tau_n, bv_taun),
+        polarimetric_vec_dm0(H_pi_n, bv_taun),
+    )
+
+    return taup_s, spatial(H_tau_p)[0].unit(), taun_s, spatial(H_tau_n)[0].unit()
