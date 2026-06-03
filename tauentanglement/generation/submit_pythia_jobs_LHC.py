48ff5510d6d632e06745f4aea40c9d19f9f7f2a8
Daniel Winterbottom <daniel.winterbottom@cern.ch>
2026-06-03 21:41:27 +0100

    updating options to submit lhe events

--- tauentanglement/generation/submit_pythia_jobs_LHC.py
+++ tauentanglement/generation/submit_pythia_jobs_LHC.py
@@ -6,7 +6,7 @@ parser.add_argument('--input', '-i', help= 'Name of input directory which should
 parser.add_argument('--cmnd_file', '-c', help= 'Pythia8 command file')
 parser.add_argument('--N_jobs', '-n', help= 'Number of job to submit', default=-1, type=int)
 parser.add_argument('--job_name', '-j', help= 'Name of job to submit', default='pythia')
-parser.add_argument('--no_lhe', default='1', help= 'If set, do not use LHE files as input to Pythia8 i.e generate ee->tautau events directly in Pythia8')
+parser.add_argument('--no_lhe', default=1, help= 'If set, do not use LHE files as input to Pythia8 i.e generate ee->tautau events directly in Pythia8', type=int)
 parser.add_argument('--extra', default='', help= 'Extra options for the shower_events.py script')
 parser.add_argument('--start_seed', default=0, help= 'First seed number to use for the jobs, seeds will be assigned sequentially starting from this number', type=int)
 
@@ -43,19 +43,21 @@ SEED=$(($2 + {start_seed}))\n\
 echo "Seed = $SEED"\n\
 cd {current_dir}/\n' % vars()
 
-if not args.no_lhe:
-    out_string += f'gunzip {args.input}/job_output_$SEED/events_$SEED.lhe.gz\n\
+if args.no_lhe == 0:
+    out_string += f'rm {args.job_name}/job_output_$SEED/*.{{root,hepmc}}\n\
+gunzip {args.input}/job_output_$SEED/events_$SEED.lhe.gz\n\
 python tauentanglement/generation/shower_events.py -c {args.cmnd_file}  -i {args.input}/job_output_$SEED/events_$SEED.lhe -o {args.input}/job_output_$SEED/pythia_events_$SEED.hepmc --seed $SEED -n -1 {args.extra}\n\
 ' % vars()
 else:
     nperjob = 10000
     out_string += f'mkdir -p {args.job_name}/job_output_$SEED/\n\
 rm {args.job_name}/job_output_$SEED/*.{{root,hepmc}}\n\
-python tauentanglement/generation/shower_events.py -c {args.cmnd_file} -n {nperjob} -o {args.job_name}/job_output_$SEED/pythia_events_$SEED.hepmc --seed $SEED {args.extra}\n\
-DelphesHepMC3 tauentanglement/generation/configs/delphes_card_CMS.tcl {args.job_name}/job_output_$SEED/delphes_output_$SEED.root {args.job_name}/job_output_$SEED/pythia_events_$SEED.hepmc\n\
+python tauentanglement/generation/shower_events.py -c {args.cmnd_file} -n {nperjob} -o {args.job_name}/job_output_$SEED/pythia_events_$SEED.hepmc --seed $SEED {args.extra}\n'
+
+out_string += f'DelphesHepMC3 tauentanglement/generation/configs/delphes_card_CMS.tcl {args.job_name}/job_output_$SEED/delphes_output_$SEED.root {args.job_name}/job_output_$SEED/pythia_events_$SEED.hepmc\n\
 python tauentanglement/generation/run_delphes.py -i {args.job_name}/job_output_$SEED/delphes_output_$SEED.root -o {args.job_name}/job_output_$SEED/reco_events_$SEED.root\n\
 rm {args.job_name}/job_output_$SEED/pythia_events_$SEED.hepmc\n\
-rm {args.job_name}/job_output_$SEED/pythia_events_$SEED.root\n\
+#rm {args.job_name}/job_output_$SEED/pythia_events_$SEED.root\n\
 rm {args.job_name}/job_output_$SEED/delphes_output_$SEED.root'
 
 os.system('mkdir -p jobs')
